06399876cc3eace667f3240d8d45f7acf76d3572
Stanley Wong <stanley-wong@lambdastudents>
2021-01-22 10:56:10 -0800

    Create a CodeGrade submission

--- README.md
+++ README.md
@@ -23,10 +23,13 @@ Commit your code regularly and meaningfully. This helps both you (in case you ev
 Be prepared to demonstrate your understanding of this week's concepts by answering questions on the following topics. You might prepare by writing down your own answers before hand.
 
 1. How would you describe acessibility on the web to someone new to programming?
+Web accessibility are designed so that people with disablities can browse the web.
 2. Talk about 3 different things you can do to ensure your website is accessible. 
+3 things that I can ensure on my website to make it accessible is adding alt text to my images, enable resizable text and coloring of my text or headers to make it easier on human's eyes.  
 3. How would you explain the concept of a variable to someone new to programming?
+Variable is used to store information, their purpose is to label and store data in memory.
 4. What is the purpose of using functions in code?
-
+Purpose of a function in code is to check for errors in your web and to keep your code organized.
 You are expected to be able to answer questions in these areas. Your responses contribute to your Sprint Challenge grade. 
 
 ## Instructions
@@ -47,8 +50,8 @@ NOTE: Tests will run for the JavaScript portion of this challenge only
 
 *Before you jump in, take 10 minutes to review the code that has already been provided for you. Take time to see how the home page was built. During this time, [Review the provided design files](design/). You have been provided all content necessary in the [index.html file](index.html) and basic styling in the [index.css file](css/index.css).*
 
-* [ ] Add a viewport meta tag to the head of your index.html page.
-* [ ] Add responsive breakpoints to your code for 500px such that your styles match the [mobile design file](design/Mobile.png).
+* [x ] Add a viewport meta tag to the head of your index.html page.
+* [x ] Add responsive breakpoints to your code for 500px such that your styles match the [mobile design file](design/Mobile.png).
 
 ### Task 2b: Minimum Viable Product - JavaScript
 
--- index.js
+++ index.js
@@ -208,16 +208,18 @@ Practice accessing data above by console.log-ing following items:
 (no functions needed) */
 
 //(1) Name of the first artist (0th index) in the array
+console.log(artists[0].name);
 
 
 //(2) Bio of the third artist (2nd index) in the array 
+console.log(artists[2].bio);
 
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 2: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
 (no function needed) 
 There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. Use an array method to fix this issue and console.log() to check your work. */
-
+artists[8].name="Vincent Van Gogh";
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 3: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀  
@@ -228,8 +230,8 @@ There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is current
  
  Example, if getArtistByIndex is invoked with the artists array and the number 0, it will return `the artist at index 0 is Amedeo Modigliani` */
 
-function getArtistByIndex(/*Your Code Here*/) {
-  /*Your Code Here*/
+function getArtistByIndex(arr,i) {
+  return 'the artist at index ${i} is ${arr[i]name}'
 }  
 
 
@@ -242,8 +244,16 @@ Use get20s to do the following:
 Example born in 1901 and died in 1959 - included -- born in 1889 and died in 1925 not included
 If correct, the function should return ["Salvador Dali", "Frida Kahlo"]*/
 
-function get20s(/*Your Code Here*/){
-  /*Your Code Here*/
+
+function get20s(array) {
+  twenties = []
+  for (i = 0; i < array.length; i++) {
+      if (array[i].years.substring(0, 4) >= 1900 && array[i].years.slice(-4) <= 2000) {
+          twenties.push(array[i].name);
+      }
+  }
+  return twenties;
+  console.log(twenties);
 }
 
 
@@ -257,8 +267,9 @@ function get20s(/*Your Code Here*/){
  
  For example, if removeArtist is invoked with the artists array and the number 0, it will remove Amedeo Modigliani from our dataset and return the number 19. */
 
-function removeArtist(/*Your Code Here*/){
-   /*Your Code Here*/
+function removeArtist(arr,i){
+   arr.splice(i,1);
+   return arr.length;
 }
    
 
@@ -278,8 +289,16 @@ Use addArtist to do the following:
 
 Example: addArtist(artists) should return the artists array with the above object added to the end of the array. */
 
-function addArtist(/*Your Code Here*/){
-    /*Your Code Here*/
+function addArtist(arr){
+    arr.push({
+      id:29
+      name:"Stanley Wong",
+      years:"1991 - 22",
+      genre: "Web Design",
+      nationality: "asian",
+      bio:"My hero is Kobe Bryant."
+    } );
+    return arr;
   }
 
   
@@ -291,8 +310,14 @@ Use lotsOfArt to do the following:
 
 For example lotsOfArt(artists); will return ["Amedeo Modigliani", "Rene Magritte", ... "Albrecht Dürer"]*/
 
-function lotsOfArt(/*Your Code Here*/){
-  /*Your Code Here*/
+function lotsOfArt(artists){
+  let paintALL =[];
+  for(let i =0; i <artists.length; i++){
+    if(arists[i].paintings>100){
+      paintALL.push(artists[i].name);
+    }
+  }
+  return paintALL;
 }
 
 
